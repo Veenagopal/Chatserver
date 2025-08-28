77f687a7a93f37443cfb8a11aad62b8ed0ef46b6
vindya <vindyagopal8@gmail.com>
2025-08-28 13:16:20 +0530

    session3

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Depends, HTTPExcept
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 
 from database import SessionLocal, init_db, DATABASE_URL, engine
 from models import Base, User, PendingMessage, SessionKey, PendingSession
